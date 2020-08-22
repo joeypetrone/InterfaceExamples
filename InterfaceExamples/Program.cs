62c92a4553caa657870a3c3247830a0045537dee
Joey Petrone <joey.petrone@gmail.com>
2020-08-22 13:02:25 -0500

    Add project files.

---
+++ InterfaceExamples/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace InterfaceExamples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
